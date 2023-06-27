9bfef6e76f3f06d0a84cd454c08f3557fe6d03ec
RacquelNinaDennison <racquelnina@gmail.com>
2023-06-27 10:51:15 +0200

    Changed the tick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import ChecklistRtlIcon from "@mui/icons-material/ChecklistRtl";
 
 const Header = (props) => {
 	return (
 		<header>
-			<h1>
-				<ChecklistRtlIcon />
-				To do list
-			</h1>
+			<h1>To do list</h1>
 			<button onClick={props.logoutHandler}>Logout</button>
 		</header>
 	);
